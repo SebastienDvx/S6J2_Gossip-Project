e22e0822411a723d4a536843dc61e38b69d69048
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-14 00:12:20 +0100

    [ADD] Code d'accès

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :sign_up_code
+  validates :sign_up_code,
+  on: :create,
+  presence: true,
+  inclusion: { in: ["SESSION3"] }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
